7a2f966a32a219076706eaa7038378701052a9af
Ravikumar Soni <ravirsoni@Ravikumars-MBP.home>
2016-11-29 19:24:11 -0800

    Initial commit

---
+++ app/src/main/java/com/ravirsoni/intentlession/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.ravirsoni.intentlession;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void openFirstPage (View view){
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
